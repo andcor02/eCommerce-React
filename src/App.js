6924bc40d6c1a0acc890850687b5cce06a8cd3ab
andrea.corrado <andrea.corrado@nbt.ag>
2021-09-06 00:03:48 +0200

    removed switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ChakraProvider, theme } from '@chakra-ui/react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import NavigationBar from './components/Nav/NavigationBar'
 import Home from './pages/Home'
 
