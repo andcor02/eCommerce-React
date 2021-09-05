953a56ae290a6d311099ed1d7a76f856adbcd5e1
andrea.corrado <andrea.corrado@nbt.ag>
2021-09-05 12:53:01 +0200

    adding responsiveness

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,6 +8,7 @@ import {
   Heading,
   HStack,
   Stack,
+  useMediaQuery,
 } from '@chakra-ui/react'
 import * as React from 'react'
 import Logo from '../logo.png'
@@ -16,20 +17,28 @@ import GoogleStore from '../googleStore.png'
 import Splash from '../DES_1135_DE-eng-01.png'
 
 const Hero = () => {
+  const [isLargerThan1100] = useMediaQuery('(min-width: 1100px)')
   return (
     <Box
       w='full'
-      h='45rem'
+      hMin='45rem'
       pt='5rem'
-      pb='2rem'
+
       pr='5rem'
       pl='5rem'
       bg='rgba(237, 107, 167)'
       overflow='hidden'
     >
-      <Stack position='relative' direction='row' >
-        <Box w='50%'>
-          <Image mb='80px' w='7rem' src={Logo} alt='Flink' />
+      <Stack
+        position='relative'
+        direction={isLargerThan1100 ? 'row' : 'column'}
+      >
+        <Stack
+          direction='column'
+          spacing='4rem'
+          w={isLargerThan1100 ? '50%' : '100%'}
+        >
+          <Image w='7rem' src={Logo} alt='Flink' />
           <Heading color='white' mb='40px' as='h1' size='3xl'>
             Groceries delivered in 10 minutes
           </Heading>
@@ -38,12 +47,20 @@ const Hero = () => {
             fruit and vegetables of the highest quality directly to your home.
             Within 10 minutes and at supermarket prices.
           </Text>
-          <HStack mt='80px' align='stretch' spacing='2rem'>
+          <HStack align='stretch' spacing='2rem' pb='2rem'>
             <Image w='14rem' src={AppStore} alt='AppStore' />
             <Image w='14rem' src={GoogleStore} alt='GoogleStore' />
           </HStack>
-        </Box>
-        <Image w='50rem' position='absolute' right='-13rem' top='1.4rem' src={Splash} alt='Splash' />
+        </Stack>
+
+        <Image
+          w='50rem'
+          position={isLargerThan1100 ? 'absolute' : ''}
+          right='-13rem'
+          top='1.4rem'
+          src={Splash}
+          alt='Splash'
+        />
       </Stack>
     </Box>
   )
