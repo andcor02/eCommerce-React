5fc67e141cbaab194602169fed38711a38ed9d9a
andrea.corrado <andrea.corrado@nbt.ag>
2021-09-05 09:57:58 +0200

    added maps

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   ChakraProvider,
   Box,
@@ -7,19 +7,48 @@ import {
   VStack,
   Code,
   Grid,
+  Flex,
   Heading,
+  Button,
   Stack,
+  Image,
   theme,
 } from '@chakra-ui/react'
-import Hero from './components/Hero/Hero'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Hero from '.././components/Hero/Hero'
+import data from '.././data/cities.json'
 
 const Home = () => {
+  const { cities } = data
+  const [selectedCity, setSelectedCity] = useState(cities[0])
   return (
     <>
       <Hero />
       <Box p='5rem'>
         <Stack direction='column'>
-          <Heading>Explore our cities</Heading>
+          <Box>
+            <Heading>Explore our cities</Heading>
+            <Stack>
+              {cities.map((items) => {
+                return (
+                  <Button onClick={() => setSelectedCity(items)} minW='10rem'>
+                    <Text position='absolute' left='4'>
+                      {items.name}
+                    </Text>
+                    <ChevronRightIcon position='absolute' right='4' />
+                  </Button>
+                )
+              })}
+            </Stack>
+          </Box>
+          <Box>
+            <Image
+              w='25rem'
+              position='absolute'
+              src={selectedCity.url}
+              alt='Map'
+            />
+          </Box>
         </Stack>
       </Box>
     </>
